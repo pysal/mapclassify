1a0d2196352d518fdf685d0a102caf00b7a5d1ec
James Gaboardi <jgaboardi@gmail.com>
2022-11-02 21:46:44 -0400

    break out classify tests

--- mapclassify/tests/test_classify.py
+++ mapclassify/tests/test_classify.py
@@ -2,7 +2,6 @@ from geopandas import gpd
 from libpysal import examples
 
 import mapclassify
-from mapclassify import classify
 
 
 def _assertions(a, b):
@@ -12,74 +11,80 @@ def _assertions(a, b):
     assert a.counts.all() == b.counts.all()
 
 
-def test_classify():
-    # data
-    link_to_data = examples.get_path("columbus.shp")
-    gdf = gpd.read_file(link_to_data)
-    x = gdf["HOVAL"].values
+class TestClassify:
+    def setup_method(self):
+        link_to_data = examples.get_path("columbus.shp")
+        gdf = gpd.read_file(link_to_data)
+        self.x = gdf["HOVAL"].values
 
-    # box_plot
-    a = classify(x, "box_plot")
-    b = mapclassify.BoxPlot(x)
-    _assertions(a, b)
+    def test_box_plot(self):
+        a = mapclassify.classify(self.x, "box_plot")
+        b = mapclassify.BoxPlot(self.x)
+        _assertions(a, b)
 
-    # EqualInterval
-    a = classify(x, "EqualInterval", k=3)
-    b = mapclassify.EqualInterval(x, k=3)
-    _assertions(a, b)
+    def test_equal_interval(self):
+        a = mapclassify.classify(self.x, "EqualInterval", k=3)
+        b = mapclassify.EqualInterval(self.x, k=3)
+        _assertions(a, b)
 
-    # FisherJenks
-    a = classify(x, "FisherJenks", k=3)
-    b = mapclassify.FisherJenks(x, k=3)
-    _assertions(a, b)
+    def test_fisher_jenks(self):
+        a = mapclassify.classify(self.x, "FisherJenks", k=3)
+        b = mapclassify.FisherJenks(self.x, k=3)
+        _assertions(a, b)
 
-    a = classify(x, "FisherJenksSampled", k=3, pct_sampled=0.5, truncate=False)
-    b = mapclassify.FisherJenksSampled(x, k=3, pct=0.5, truncate=False)
-    _assertions(a, b)
+    def test_fisher_jenks_sampled(self):
+        a = mapclassify.classify(
+            self.x, "FisherJenksSampled", k=3, pct_sampled=0.5, truncate=False
+        )
+        b = mapclassify.FisherJenksSampled(self.x, k=3, pct=0.5, truncate=False)
+        _assertions(a, b)
 
-    # headtail_breaks
-    a = classify(x, "headtail_breaks")
-    b = mapclassify.HeadTailBreaks(x)
-    _assertions(a, b)
+    def test_headtail_breaks(self):
+        a = mapclassify.classify(self.x, "headtail_breaks")
+        b = mapclassify.HeadTailBreaks(self.x)
+        _assertions(a, b)
 
-    # quantiles
-    a = classify(x, "quantiles", k=3)
-    b = mapclassify.Quantiles(x, k=3)
-    _assertions(a, b)
+    def test_quantiles(self):
+        a = mapclassify.classify(self.x, "quantiles", k=3)
+        b = mapclassify.Quantiles(self.x, k=3)
+        _assertions(a, b)
 
-    # percentiles
-    a = classify(x, "percentiles", pct=[25, 50, 75, 100])
-    b = mapclassify.Percentiles(x, pct=[25, 50, 75, 100])
-    _assertions(a, b)
+    def test_percentiles(self):
+        a = mapclassify.classify(self.x, "percentiles", pct=[25, 50, 75, 100])
+        b = mapclassify.Percentiles(self.x, pct=[25, 50, 75, 100])
+        _assertions(a, b)
 
-    # JenksCaspall
-    a = classify(x, "JenksCaspall", k=3)
-    b = mapclassify.JenksCaspall(x, k=3)
-    _assertions(a, b)
+    def test_jenks_caspall(self):
+        a = mapclassify.classify(self.x, "JenksCaspall", k=3)
+        b = mapclassify.JenksCaspall(self.x, k=3)
+        _assertions(a, b)
 
-    a = classify(x, "JenksCaspallForced", k=3)
-    b = mapclassify.JenksCaspallForced(x, k=3)
-    _assertions(a, b)
+    def test_jenks_caspall_forced(self):
+        a = mapclassify.classify(self.x, "JenksCaspallForced", k=3)
+        b = mapclassify.JenksCaspallForced(self.x, k=3)
+        _assertions(a, b)
 
-    a = classify(x, "JenksCaspallSampled", pct_sampled=0.5)
-    b = mapclassify.JenksCaspallSampled(x, pct=0.5)
-    _assertions(a, b)
+    def test_jenks_caspall_sampled(self):
+        a = mapclassify.classify(self.x, "JenksCaspallSampled", pct_sampled=0.5)
+        b = mapclassify.JenksCaspallSampled(self.x, pct=0.5)
+        _assertions(a, b)
 
-    # natural_breaks, max_p_classifier
-    a = classify(x, "natural_breaks")
-    b = mapclassify.NaturalBreaks(x)
-    _assertions(a, b)
+    def test_natural_breaks(self):
+        a = mapclassify.classify(self.x, "natural_breaks")
+        b = mapclassify.NaturalBreaks(self.x)
+        _assertions(a, b)
 
-    a = classify(x, "max_p", k=3, initial=50)
-    b = mapclassify.MaxP(x, k=3, initial=50)
-    _assertions(a, b)
+    def test_max_p_classifier(self):
+        a = mapclassify.classify(self.x, "max_p", k=3, initial=50)
+        b = mapclassify.MaxP(self.x, k=3, initial=50)
+        _assertions(a, b)
 
-    # std_mean
-    a = classify(x, "std_mean", multiples=[-1, -0.5, 0.5, 1])
-    b = mapclassify.StdMean(x, multiples=[-1, -0.5, 0.5, 1])
-    _assertions(a, b)
+    def test_std_mean(self):
+        a = mapclassify.classify(self.x, "std_mean", multiples=[-1, -0.5, 0.5, 1])
+        b = mapclassify.StdMean(self.x, multiples=[-1, -0.5, 0.5, 1])
+        _assertions(a, b)
 
-    # user_defined
-    a = classify(x, "user_defined", bins=[20, max(x)])
-    b = mapclassify.UserDefined(x, bins=[20, max(x)])
-    _assertions(a, b)
+    def test_user_defined(self):
+        a = mapclassify.classify(self.x, "user_defined", bins=[20, max(self.x)])
+        b = mapclassify.UserDefined(self.x, bins=[20, max(self.x)])
+        _assertions(a, b)
