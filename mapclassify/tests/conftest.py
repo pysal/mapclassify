77eb69b43e1e8d963b5d2b532c0dc44b7be1795e
James Gaboardi <jgaboardi@gmail.com>
2026-02-22 20:26:44 -0500

    light relaxation of RMS for image testing

--- mapclassify/tests/conftest.py
+++ mapclassify/tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 def pytest_configure(config):  # noqa: ARG001
     """PyTest session attributes, methods, etc."""
 
-    pytest.image_comp_kws = {"extensions": ["png"], "tol": 0.05, "remove_text": True}
+    pytest.image_comp_kws = {"extensions": ["png"], "tol": 0.35, "remove_text": True}
     pytest.image_comp_kws_legend_text = {
         "extensions": ["png"],
         "tol": 2.8,
